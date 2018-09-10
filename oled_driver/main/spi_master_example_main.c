4aefd4a28938303a695f2ae100cafa68c950c43b
kyChu <hello.kychu@gmail.com>
2018-09-10 19:21:14 +0800

    change pin connection

--- oled_driver/main/spi_master_example_main.c
+++ oled_driver/main/spi_master_example_main.c
@@ -31,13 +31,13 @@
 */
 
 #define PIN_NUM_MISO 25
-#define PIN_NUM_MOSI 23
+#define PIN_NUM_MOSI 18
 #define PIN_NUM_CLK  19
 #define PIN_NUM_CS   22
 
-#define PIN_NUM_DC   21
-#define PIN_NUM_RST  18
-#define PIN_NUM_BCKL 5
+#define PIN_NUM_DC   4
+#define PIN_NUM_RST  5
+#define PIN_NUM_BCKL 2
 
 //To speed up transfers, every SPI transfer sends a bunch of lines. This define specifies how many. More means more memory use,
 //but less overhead for setting up / finishing transfers. Make sure 240 is dividable by this.
