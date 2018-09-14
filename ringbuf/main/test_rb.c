b1bd6af7bc8c5bb6ffa988e5b919750bdae18c81
kyChu <hello.kychu@gmail.com>
2018-09-14 15:46:46 +0800

    ringbuf send receive test

--- ringbuf/main/test_rb.c
+++ ringbuf/main/test_rb.c
@@ -16,7 +16,39 @@
 
 static const char *TAG = "rb_test";
 
+static char tx_item[] = "kychu test item.";
+
 void app_main(void)
 {
 	ESP_LOGI(TAG, "start ringbuffer test.");
+	//Create ring buffer;
+	RingbufHandle_t buf_handle;
+	buf_handle = xRingbufferCreate(1028, RINGBUF_TYPE_NOSPLIT);
+	if(buf_handle == NULL) {
+		ESP_LOGE(TAG, "Failed to create ring buffer.");
+	}
+
+	//Send an item
+	UBaseType_t res = xRingbufferSend(buf_handle, tx_item, sizeof(tx_item), pdMS_TO_TICKS(1000));
+	if(res != pdTRUE) {
+		ESP_LOGE(TAG, "Failed to send item.");
+	}
+
+	//Receive on item from no-split ring buffer
+	size_t item_size;
+	char *item = (char *)xRingbufferReceive(buf_handle, &item_size, pdMS_TO_TICKS(1000));
+
+	//Check received item
+	if(item != NULL) {
+		//Print item
+		for(int i = 0; i < item_size; i ++) {
+			printf("%c", item[i]);
+		}
+		printf("\n");
+		//Return Item
+		vRingbufferReturnItem(buf_handle, (void *)item);
+	} else {
+		//Failed to receive item
+		ESP_LOGE(TAG, "Failed to receive item.");
+	}
 }
