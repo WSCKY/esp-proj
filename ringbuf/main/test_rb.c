c70c25f00767f56b8cbe0a03428b1ab504b3c227
kyChu <hello.kychu@gmail.com>
2018-09-14 17:38:10 +0800

    test for byte ring buffer

--- ringbuf/main/test_rb.c
+++ ringbuf/main/test_rb.c
@@ -17,13 +17,13 @@
 static const char *TAG = "rb_test";
 
 static char tx_item[] = "kychu test item.";
-static char tx_item_1[] = "kychu test item - 1.";
+static char tx_item_1[] = "kychu test item - 1."; /* 20 Bytes. */
 static char tx_item_2[] = "kychu test item - 2.";
 static char tx_item_3[] = "kychu test item - 3.";
 
 void app_main(void)
 {
-	ESP_LOGI(TAG, "start ringbuffer test.");
+	ESP_LOGI(TAG, "ring buffer test, RINGBUF_TYPE_NOSPLIT");
 	//Create ring buffer;
 	RingbufHandle_t buf_handle;
 	buf_handle = xRingbufferCreate(1028, RINGBUF_TYPE_NOSPLIT);
@@ -56,6 +56,7 @@ void app_main(void)
 	}
 	vRingbufferDelete(buf_handle);
 
+	ESP_LOGI(TAG, "ring buffer test, RINGBUF_TYPE_ALLOWSPLIT");
 	buf_handle = xRingbufferCreate(80, RINGBUF_TYPE_ALLOWSPLIT); /* 80 bytes. */
 	if(buf_handle == NULL) {
 		ESP_LOGE(TAG, "Failed to create ring buffer.");
@@ -68,7 +69,7 @@ void app_main(void)
 	if(res != pdTRUE) {
 		ESP_LOGE(TAG, "Failed to send item 2.");
 	}
-	res = xRingbufferSend(buf_handle, tx_item_3, sizeof(tx_item_3), pdMS_TO_TICKS(1000)); /* no enough space. */
+	res = xRingbufferSend(buf_handle, tx_item_3, sizeof(tx_item_3), pdMS_TO_TICKS(1000)); /* it will no enough space. */
 	if(res != pdTRUE) {
 		ESP_LOGE(TAG, "Failed to send item 3.");
 	}
@@ -96,4 +97,49 @@ void app_main(void)
 		//Failed to receive item
 		ESP_LOGE(TAG, "Failed to receive item.");
 	}
+	vRingbufferDelete(buf_handle);
+
+	ESP_LOGI(TAG, "ring buffer test, RINGBUF_TYPE_BYTEBUF");
+	buf_handle = xRingbufferCreate(50, RINGBUF_TYPE_BYTEBUF); /* 50 bytes. */
+	ESP_LOGI(TAG, "Max Item Size: %d.", xRingbufferGetMaxItemSize(buf_handle));
+	ESP_LOGI(TAG, "Current Free Size: %d.", xRingbufferGetCurFreeSize(buf_handle));
+	res = xRingbufferSend(buf_handle, tx_item_1, sizeof(tx_item_1), pdMS_TO_TICKS(1000));
+	if(res != pdTRUE) {
+		ESP_LOGE(TAG, "Failed to send item 1.");
+	}
+	ESP_LOGI(TAG, "Max Item Size: %d.", xRingbufferGetMaxItemSize(buf_handle));
+	ESP_LOGI(TAG, "Current Free Size: %d.", xRingbufferGetCurFreeSize(buf_handle));
+	res = xRingbufferSend(buf_handle, tx_item_2, sizeof(tx_item_2), pdMS_TO_TICKS(1000));
+	if(res != pdTRUE) {
+		ESP_LOGE(TAG, "Failed to send item 2.");
+	}
+	ESP_LOGI(TAG, "Max Item Size: %d.", xRingbufferGetMaxItemSize(buf_handle));
+	ESP_LOGI(TAG, "Current Free Size: %d.", xRingbufferGetCurFreeSize(buf_handle));
+	res = xRingbufferSend(buf_handle, tx_item_3, sizeof(tx_item_3), pdMS_TO_TICKS(1000)); /* it will no enough space. */
+	if(res != pdTRUE) {
+		ESP_LOGE(TAG, "Failed to send item 3.");
+	}
+	ESP_LOGI(TAG, "Max Item Size: %d.", xRingbufferGetMaxItemSize(buf_handle));
+	ESP_LOGI(TAG, "Current Free Size: %d.", xRingbufferGetCurFreeSize(buf_handle));
+
+	//Receive data from byte buffer
+	item = (char *)xRingbufferReceiveUpTo(buf_handle, &item_size, pdMS_TO_TICKS(1000), 60);
+	ESP_LOGI(TAG, "Max Item Size: %d.", xRingbufferGetMaxItemSize(buf_handle));
+	ESP_LOGI(TAG, "Current Free Size: %d.", xRingbufferGetCurFreeSize(buf_handle));
+
+	//Check received data
+	if (item != NULL) {
+		//Print item
+		for (int i = 0; i < item_size; i++) {
+			printf("%c", item[i]);
+		}
+		printf("\n");
+		//Return Item
+		vRingbufferReturnItem(buf_handle, (void *)item); /* flush data at here. */
+	} else {
+		//Failed to receive item
+		printf("Failed to receive item\n");
+	}
+	ESP_LOGI(TAG, "Max Item Size: %d.", xRingbufferGetMaxItemSize(buf_handle));
+	ESP_LOGI(TAG, "Current Free Size: %d.", xRingbufferGetCurFreeSize(buf_handle));
 }
