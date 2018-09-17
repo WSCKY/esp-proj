62454cc8b9943522f36a1478b5e19b511fc57afd
kyChu <hello.kychu@gmail.com>
2018-09-17 17:04:50 +0800

    fix path error

--- imgview/components/drivers/sdcard/component.mk
+++ imgview/components/drivers/sdcard/component.mk
@@ -3,6 +3,6 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 #component standalone mode
-COMPONENT_ADD_INCLUDEDIRS : = .
+COMPONENT_ADD_INCLUDEDIRS := .
 COMPONENT_SRCDIRS := .
 
