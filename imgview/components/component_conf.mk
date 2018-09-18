14d8d7cb2c4c2b19b34ec9712b662c001aaaa472
kyChu <hello.kychu@gmail.com>
2018-09-18 17:52:16 +0800

    add image decoder mdule

--- imgview/components/component_conf.mk
+++ imgview/components/component_conf.mk
@@ -4,5 +4,6 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
 USER_DIRS += $(USER_PATH)/components/drivers/
+USER_DIRS += $(USER_PATH)/components/middlewares/
 
 EXTRA_COMPONENT_DIRS += $(USER_DIRS)
