98412dc12b89747e3e0d10a2877a1d2144e6d6cf
kyChu <hello.kychu@gmail.com>
2018-09-21 16:16:55 +0800

    add LcdSize_t input parameter

--- imgview/components/middlewares/picDec/imgDecoder.h
+++ imgview/components/middlewares/picDec/imgDecoder.h
@@ -8,15 +8,19 @@
 #include "bmpDec.h"
 #include "jpgDec.h"
 
+#define LCD_WIDTH_DEFAULT      128
+#define LCD_HEIGHT_DEFAULT     160
+
 #ifdef __cplusplus
 
 class imgDecoder {
 private:
 	const char *path;
+	LcdSize_t LcdSize;
 	pDrawPrepare_t pDrawInit;
 	pFillScreen_t pFillData;
 public:
-	imgDecoder(const char *file, pDrawPrepare_t pDrawPrepare, pFillScreen_t pFillScreen);
+	imgDecoder(pDrawPrepare_t pDrawPrepare, pFillScreen_t pFillScreen, LcdSize_t size = {LCD_WIDTH_DEFAULT, LCD_HEIGHT_DEFAULT});
 	virtual ~imgDecoder();
 	esp_err_t decodeBMP(const char *file);
 	esp_err_t decodeJPG(const char *file);
