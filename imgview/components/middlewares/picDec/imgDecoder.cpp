84f62563c135806d43370709b70209b122938fa3
kyChu <hello.kychu@gmail.com>
2018-09-21 16:18:26 +0800

    check file pointer

--- imgview/components/middlewares/picDec/imgDecoder.cpp
+++ imgview/components/middlewares/picDec/imgDecoder.cpp
@@ -3,9 +3,9 @@
 
 #include "imgDecoder.h"
 
-imgDecoder::imgDecoder(const char *file, pDrawPrepare_t pDrawPrepare, pFillScreen_t pFillScreen)
+imgDecoder::imgDecoder(pDrawPrepare_t pDrawPrepare, pFillScreen_t pFillScreen, LcdSize_t size)
 {
-	path = file;
+	LcdSize = size;
 	pDrawInit = pDrawPrepare;
 	pFillData = pFillScreen;
 }
@@ -17,12 +17,14 @@ imgDecoder::~imgDecoder()
 
 esp_err_t imgDecoder::decodeBMP(const char *file)
 {
-	if(file != NULL) path = file;
+	if(file == NULL) return ESP_ERR_INVALID_ARG;
+	path = file;
 	return bmp_decode(path, pDrawInit, pFillData);
 }
 
 esp_err_t imgDecoder::decodeJPG(const char *file)
 {
-	if(file != NULL) path = file;
+	if(file == NULL) return ESP_ERR_INVALID_ARG;
+	path = file;
 	return jpg_decode(path, pDrawInit, pFillData);
 }
