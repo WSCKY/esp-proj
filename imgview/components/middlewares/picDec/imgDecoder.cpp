947b5a73d6176b2d486b4b61dc42af4c28926fdb
kyChu <hello.kychu@gmail.com>
2018-09-22 22:25:41 +0800

    add image type process function

--- imgview/components/middlewares/picDec/imgDecoder.cpp
+++ imgview/components/middlewares/picDec/imgDecoder.cpp
@@ -1,7 +1,16 @@
 #include "string.h"
 #include "stdio.h"
+#include "esp_log.h"
 
 #include "imgDecoder.h"
+
+ImgSuffix_t ImgTypes[] = {
+		{".bmp", Img_BMP}, {".BMP", Img_BMP},
+		{".jpg", Img_JPG}, {".JPG", Img_JPG},
+		{".jpeg", Img_JPG}, {".JPEG", Img_JPG},
+		{".gif", Img_GIF}, {".GIF", Img_GIF},
+		{"Unknow", Img_Unknow},
+};
 
 imgDecoder::imgDecoder(pDrawPrepare_t pDrawPrepare, pFillScreen_t pFillScreen, LcdSize_t size)
 {
@@ -13,6 +22,46 @@ imgDecoder::imgDecoder(pDrawPrepare_t pDrawPrepare, pFillScreen_t pFillScreen, L
 imgDecoder::~imgDecoder()
 {
 
+}
+
+bool imgDecoder::strcmp(const char *p1, const char *p2)
+{
+	while((*p1 != 0) && (*p2 != 0)) {
+		if(*p1 != *p2) return false;
+		p1 ++; p2 ++;
+	}
+	if((*p1 != 0) || (*p2 != 0)) return false;
+	return true;
+}
+
+ImgType_t imgDecoder::checkType(const char *file)
+{
+	char *pointer = (char *)file + strlen(file);
+	int len = 5;
+	while(len --) {
+		if(*(-- pointer) == '.') break;
+	}
+	if(*pointer == '.') {
+		len = 0;
+		while(ImgTypes[len].type != Img_Unknow) {
+			if(strcmp(ImgTypes[len].suffix, pointer)) {
+				return ImgTypes[len].type;
+			}
+			len ++;
+		}
+		return Img_Unknow;
+	} else {
+		return Img_Unknow;
+	}
+}
+
+const char *imgDecoder::imgType2String(ImgType_t type)
+{
+	int i = 0;
+	while(ImgTypes[i].type != type && ImgTypes[i].type != Img_Unknow) {
+		i ++;
+	}
+	return ImgTypes[i].suffix;
 }
 
 esp_err_t imgDecoder::decodeBMP(const char *file)
--- imgview/components/middlewares/picDec/imgDecoder.h
+++ imgview/components/middlewares/picDec/imgDecoder.h
@@ -11,6 +11,19 @@
 #define LCD_WIDTH_DEFAULT      128
 #define LCD_HEIGHT_DEFAULT     160
 
+typedef enum {
+	Img_BMP = 0,
+	Img_JPG = 1,
+	Img_GIF = 2,
+
+	Img_Unknow = 0xF,
+} ImgType_t;
+
+typedef struct {
+	const char *suffix;
+	ImgType_t type;
+} ImgSuffix_t;
+
 #ifdef __cplusplus
 
 class imgDecoder {
@@ -19,9 +32,12 @@ private:
 	LcdSize_t LcdSize;
 	pDrawPrepare_t pDrawInit;
 	pFillScreen_t pFillData;
+	bool strcmp(const char *p1, const char *p2);
 public:
 	imgDecoder(pDrawPrepare_t pDrawPrepare, pFillScreen_t pFillScreen, LcdSize_t size = {LCD_WIDTH_DEFAULT, LCD_HEIGHT_DEFAULT});
 	virtual ~imgDecoder();
+	ImgType_t checkType(const char *file);
+	const char *imgType2String(ImgType_t type);
 	esp_err_t decodeBMP(const char *file);
 	esp_err_t decodeJPG(const char *file);
 };
