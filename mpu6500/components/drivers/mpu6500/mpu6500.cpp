cf4df2670438739c6fbcf3e2be73f9522e5451b1
kyChu <hello.kychu@gmail.com>
2018-09-30 16:38:45 +0800

    verify bit shift

--- mpu6500/components/drivers/mpu6500/mpu6500.cpp
+++ mpu6500/components/drivers/mpu6500/mpu6500.cpp
@@ -244,22 +244,22 @@ esp_err_t MPU6500::set_gyr_dlpf(gyr_dlpf_t dlpf)
 	ret = _rd_reg(MPU_REG_GYRCFG, 1);
 	if(ret == ESP_OK) {
 		uint8_t tmp = _rx_buf[1];
-		tmp &= ~DLPF_BYPASS_Msk;
+		tmp &= ~(DLPF_BYPASS_Msk << DLPF_BYPASS_Pos);
 		if(dlpf < GYR_DLPF_BYP_Pos) {
 			// Use DLPF.
-			tmp |= DLPF_BYPASS_Disable;
+			tmp |= (DLPF_BYPASS_Disable << DLPF_BYPASS_Pos);
 			ret = _wr_reg(MPU_REG_GYRCFG, tmp);
 			if(ret == ESP_OK) {
 				ret = _rd_reg(MPU_REG_CONFIG, 1);
 				if(ret == ESP_OK) {
 					tmp = _rx_buf[1];
-					tmp &= ~DLPF_CONFIG_Msk;
-					tmp |= dlpf;
+					tmp &= ~(DLPF_CONFIG_Msk << DLPF_CONFIG_Pos);
+					tmp |= (dlpf << DLPF_CONFIG_Pos);
 					ret = _wr_reg(MPU_REG_CONFIG, tmp);
 				}
 			}
 		} else {
-			tmp |= (dlpf - GYR_DLPF_BYP_Pos + 1);
+			tmp |= ((dlpf - GYR_DLPF_BYP_Pos + 1) << DLPF_BYPASS_Pos);
 			ret = _wr_reg(MPU_REG_GYRCFG, tmp);
 		}
 	}
